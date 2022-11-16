c89e3df439c6108151ea06ca1c81cd3ed3f1249b
danvu <d_vu@fanshaweonline.ca>
2022-11-16 13:55:43 -0500

    change api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDLWdC5YXG_-p9CUYANOM7ka-4i9FNQ_M0",
-    authDomain: "info3170-week08.firebaseapp.com",
-    projectId: "info3170-week08",
-    storageBucket: "info3170-week08.appspot.com", 
-    messagingSenderId: "915425341136",
-    appId: "1:915425341136:web:92d1e7cb0e023202aff8d5"
+    apiKey: "AIzaSyC8VR1GsIAeY2xVOLwQuMXQ6t7XDIGRz1M",
+  authDomain: "week10-a16aa.firebaseapp.com",
+  databaseURL: "https://week10-a16aa-default-rtdb.firebaseio.com",
+  projectId: "week10-a16aa",
+  storageBucket: "week10-a16aa.appspot.com",
+  messagingSenderId: "324950784256",
+  appId: "1:324950784256:web:4887dc54b25c32a1a01be5"
   }
 };
