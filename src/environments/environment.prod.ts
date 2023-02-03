9ebca3ed095686f95902bc2d3aacba88d59a45c9
Dan <d_vu@fanshaweonline.ca>
2023-02-03 08:10:00 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
- apiKey: "AIzaSyAQ7eNof0JtLv7B4325Ex52wtQ3tbzVmnU",
-  authDomain: "angular-w10-7934c.firebaseapp.com",
-  projectId: "angular-w10-7934c",
-  storageBucket: "angular-w10-7934c.appspot.com",
-  messagingSenderId: "209798534391",
-  appId: "1:209798534391:web:95f9d0b0120a15fb312ff2"
+  apiKey: "AIzaSyCpi8cVu9P9TRkRElKzFnjEa-fKjMxciVk",
+  authDomain: "angular-5b53f.firebaseapp.com",
+  databaseURL: "https://angular-5b53f-default-rtdb.firebaseio.com",
+  projectId: "angular-5b53f",
+  storageBucket: "angular-5b53f.appspot.com",
+  messagingSenderId: "264146889095",
+  appId: "1:264146889095:web:be3e2cca9a8184840b4b22"
   }
 };
